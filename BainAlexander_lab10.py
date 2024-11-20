725487c877464e60692bec6fa5aee305aaf69061
abain42 <alex.bain208@gmail.com>
2024-11-20 16:24:10 -0500

    Code finished and commented
    Part 3 was then finished by applying the function to the appropriate array rather than the test one used previously. Comments were also added for clarity

--- BainAlexander_lab10.py
+++ BainAlexander_lab10.py
@@ -1,27 +1,44 @@
 ###Lab 10 
 
 ###Part 1 ###
-import numpy as np
+import numpy as np #importing librarys
 
-def make_tridiagonal(N,b,d,a):
+#this is the function that creates the requested array, it takes the arguments
+# N = array size in collumns and rows 
+#b = to the values in the diagonal one down from the central diagonal
+#d = to the valeus in the central diagonal
+# b - to the values in the diagonal one above the central diagonal
+def make_tridiagonal(N,b,d,a): 
+    #np.eye creates a function of N by N size that assigns a diagonal values of 1
+    #and the others 0, the third argument is the specific diagonal, with 0 being the central one
+    # the number assigned to it is the # of diagonals either above or below the central one you are 
+    #trying to add 1's too, with -# being below and +# being above the central diagonal
+    #we multiply each array by the value we want each diagonal to be
+    #then we add the arrays and return the answer
     arr = np.eye(N,N,0)*d + np.eye(N,N,1)*a +  np.eye(N,N,-1)*b
     return arr 
-
+#this just calls the function as A and then we print it after
 A = make_tridiagonal(5,3,1,5)
 print(A)
 
 ### Part 2 ###
 
 from matplotlib import pyplot as plt
-
+#this is a function that returns the initial condition
+#it takes the arguments, sigma, K and x
 def make_initialcond(sigma,K,x):
+    #the initial condition can be taken as the waveform below
     a = np.exp(-((x)**2)/(2*sigma**2))*np.cos(K*x)
     return a 
+#these are constants given
 L =5
 N=500
+#this creates the spatial grid from the value of -L/2 to L/2 with N even steps
 xi = np.linspace(L/2,L/2+5,N) -5
+#this calls the function with the given paramaters
 Aofx0 = make_initialcond(sigma = 0.2,K=35,x=xi)
 
+#this plots the values of A(x,0) against the values of Xi and returns the graph
 plt.plot(xi,Aofx0)
 plt.xlabel('Values of X')
 plt.ylabel('a(x,0) values')
@@ -30,12 +47,12 @@ plt.show()
 
 
 #Part 3
-test = ([3,2],[2,3])
 
-def spectral_radius(test):
-    eigval, eigvec = np.linalg.eig(test)
+
+def spectral_radius(A):
+    eigval, eigvec = np.linalg.eig(A)
     eigvalabs = map(abs,eigval)
     eigvalabsmax = max(eigvalabs)
     return (eigvalabsmax)
 
-print(spectral_radius(test))
+print(spectral_radius(A))
