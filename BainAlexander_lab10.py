e9dbee8230f400648b9b281354e7b99960db6a54
abain42 <alex.bain208@gmail.com>
2024-11-20 15:28:07 -0500

    Wrote the function for part 2
    wrote the function for part 2 that develops a function that returns the initial condition that can be taken as a wave packet

--- BainAlexander_lab10.py
+++ BainAlexander_lab10.py
@@ -1,9 +1,23 @@
 ###Lab 10 
-import numpy 
+
+###Part 1 ###
+import numpy as np
 
 def make_tridiagonal(N,b,d,a):
-    a = numpy.eye(N,N,0)*d + numpy.eye(N,N,1)*a +  numpy.eye(N,N,-1)*b
-    return a 
+    arr = np.eye(N,N,0)*d + np.eye(N,N,1)*a +  np.eye(N,N,-1)*b
+    return arr 
 
 A = make_tridiagonal(5,3,1,5)
 print(A)
+
+### Part 2 ###
+
+
+def make_initialcond(sigma,K,x):
+    a = np.exp(-((x)**2)/(2*sigma**2))*np.cos(K,x)
+    return a 
+L =5, N=500
+xi = np.linspace(-L/2,L/2,N)
+Aofx0 = make_initialcond(sigma = 0.2,K=35,x=xi)
+
+    
